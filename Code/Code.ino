c700cfb9f9bbf1f115f0f8e46eca464448089731
shyam-king <m.shyam.tnj@gmail.com>
2019-08-24 14:19:43 +0530

    rectified timer0 control for servo at pin 6

--- Code/Code.ino
+++ Code/Code.ino
@@ -19,6 +19,125 @@
 #define middle_servo_left_angle 20
 #define middle_servo_right_angle 150
 
+
+////////////////
+// UART class //
+////////////////
+class USART
+{
+public:
+    volatile static char USART_DATA_BUFFER[32];
+    volatile static uint8_t USART_DATA_INDEX;
+    volatile static bool USART_BUFFER_READY;
+
+    /*
+        Input: none
+        Output: none
+        Logic: Initialize serial communication
+    */
+    static void init(int baud)
+    {
+        int baudRate = 1000000l / baud - 1;
+        UCSR0C = (1 << UCSZ00) | (1 << UCSZ01);               //asynchronous, 8 bit data
+        UBRR0 = baudRate;                                     //set baud rate
+        UCSR0B = (1 << RXEN0) | (1 << TXEN0) | (1 << RXCIE0); //enable RX and TX
+    }
+
+    /*
+        Input:  uint8_t data: the data to be sent
+        Output: none
+        Logic: Send 1 byte of data to the serial monitor
+    */
+    static void sendByte(uint8_t data)
+    {
+        while (!(UCSR0A & (1 << UDRE0)))
+            ;
+        UDR0 = data;
+    }
+
+    /*
+        Input:  char* data: the data (string) to be sent
+        Output: none
+        Logic: Send a string to the serial monitor
+    */
+    static void sendData(const char *data)
+    {
+        while (*data)
+        {
+            sendByte(*data);
+            data++;
+        }
+    }
+    /*
+        Input:  uint8_t* data : pointer to store received byte
+        Output: bool : true if a byte was received and put in data
+        Logic: By checking the UCSR0A.RXC0 flag availability of data in UDR0 is known,
+                if data is available, it is stored and true is returned
+        Example call: 
+        uint8_t data;
+        if (USART::getByte(&data)) {
+            //do something with data
+        } 
+    */
+    static bool getByte(uint8_t *data)
+    {
+        if (UCSR0A & (1 << RXC0))
+        {
+            *data = UDR0;
+            return true;
+        }
+        return false;
+    }
+
+    /*
+        Input:  char *dest: memory to be filled with received data (capacity 32 bytes)
+        Output: none 
+        Logic:  read the received data that is stored in the buffer
+        Example call: 
+        char data[32];
+        if (USART::bufferReady()) USART::readBuffer(data);
+    */
+    static void readBuffer(char *dest)
+    {
+        strcpy(dest, USART_DATA_BUFFER);
+        USART_DATA_INDEX = 0;
+        USART_BUFFER_READY = false;
+    }
+
+    static bool bufferReady()
+    {
+        return USART_BUFFER_READY;
+    }
+
+    static void updateBuffer(char data)
+    {
+        if (data == '\n' || data == '\r' || USART_DATA_INDEX == 31)
+        {
+            data = '\0';
+            USART_BUFFER_READY = true;
+        }
+        USART_DATA_BUFFER[USART_DATA_INDEX] = data;
+        USART_DATA_INDEX++;
+    }
+};
+
+volatile static char USART::USART_DATA_BUFFER[32] = "";
+volatile static uint8_t USART::USART_DATA_INDEX = 0;
+volatile static bool USART::USART_BUFFER_READY = false;
+
+/*
+    Function name: ISR(USART_RX_vect)
+    Logic: Whenever data is available it is appended to the buffer if the buffer has already been read 
+ */
+ISR(USART_RX_vect)
+{
+    if (USART::bufferReady() == false)
+    {
+        char data = UDR0;
+        USART::updateBuffer(data);
+    }
+}
+
 ////////////////
 // servo class //
 ////////////////
@@ -39,11 +158,14 @@ public:
         }
         if (pin == 6)
         {
-          // Timer0 in fast pwm (TOP Value as OCR0A) , non-inverting mode, 256-bit prescaling
-            DDRD |= 1<< 5;
-            TCCR0A |= (1 << WGM00) |(1<< WGM01)| (1 << COM0B1); 
-            TCCR0B |= (1 << WGM02)| (1 << CS02); 
+            // Timer0 : CTC (OCR0A)
+            DDRD |= 1<< 6;
+            TCCR0A |= (1<< WGM01);  //ctc
+            TCCR0B |=  (1 << CS02); // 256 prescalar 
             OCR0A = 125;
+            OCR0B = 0;
+            //enable interrupts 
+            TIMSK0 = (1 << 2) | (1 << 1);
         }
         if (pin == 10)
         {
@@ -69,10 +191,28 @@ servo left_servo;
 servo right_servo; // Model servos as objects of type servo
 servo middle_servo;
 
-volatile int overflows = 0;
+/*
+ * timer0 compare match a
+ */
+volatile int timer0_counter = 0;
+ISR (TIMER0_COMPA_vect) {
+  timer0_counter ++;
+  if (timer0_counter == 10) {
+    PORTD |= (1 << 6);
+    timer0_counter = 0;
+  }
+}
+
+ISR (TIMER0_COMPB_vect) {
+  if (timer0_counter == 0) {
+    PORTD &= !(1 << 6);
+  }
+}
+
 /*
 overflows counts number of overflows of Timer2
 */
+volatile int overflows = 0;
 ISR(TIMER2_OVF_vect)
 {
     ++overflows;
@@ -192,123 +332,6 @@ class bot
         }
     }
 };
-////////////////
-// UART class //
-////////////////
-class USART
-{
-public:
-    volatile static char USART_DATA_BUFFER[32];
-    volatile static uint8_t USART_DATA_INDEX;
-    volatile static bool USART_BUFFER_READY;
-
-    /*
-        Input: none
-        Output: none
-        Logic: Initialize serial communication
-    */
-    static void init(int baud)
-    {
-        int baudRate = 1000000l / baud - 1;
-        UCSR0C = (1 << UCSZ00) | (1 << UCSZ01);               //asynchronous, 8 bit data
-        UBRR0 = baudRate;                                     //set baud rate
-        UCSR0B = (1 << RXEN0) | (1 << TXEN0) | (1 << RXCIE0); //enable RX and TX
-    }
-
-    /*
-        Input:  uint8_t data: the data to be sent
-        Output: none
-        Logic: Send 1 byte of data to the serial monitor
-    */
-    static void sendByte(uint8_t data)
-    {
-        while (!(UCSR0A & (1 << UDRE0)))
-            ;
-        UDR0 = data;
-    }
-
-    /*
-        Input:  char* data: the data (string) to be sent
-        Output: none
-        Logic: Send a string to the serial monitor
-    */
-    static void sendData(const char *data)
-    {
-        while (*data)
-        {
-            sendByte(*data);
-            data++;
-        }
-    }
-    /*
-        Input:  uint8_t* data : pointer to store received byte
-        Output: bool : true if a byte was received and put in data
-        Logic: By checking the UCSR0A.RXC0 flag availability of data in UDR0 is known,
-                if data is available, it is stored and true is returned
-        Example call: 
-        uint8_t data;
-        if (USART::getByte(&data)) {
-            //do something with data
-        } 
-    */
-    static bool getByte(uint8_t *data)
-    {
-        if (UCSR0A & (1 << RXC0))
-        {
-            *data = UDR0;
-            return true;
-        }
-        return false;
-    }
-
-    /*
-        Input:  char *dest: memory to be filled with received data (capacity 32 bytes)
-        Output: none 
-        Logic:  read the received data that is stored in the buffer
-        Example call: 
-        char data[32];
-        if (USART::bufferReady()) USART::readBuffer(data);
-    */
-    static void readBuffer(char *dest)
-    {
-        strcpy(dest, USART_DATA_BUFFER);
-        USART_DATA_INDEX = 0;
-        USART_BUFFER_READY = false;
-    }
-
-    static bool bufferReady()
-    {
-        return USART_BUFFER_READY;
-    }
-
-    static void updateBuffer(char data)
-    {
-        if (data == '\n' || data == '\r' || USART_DATA_INDEX == 31)
-        {
-            data = '\0';
-            USART_BUFFER_READY = true;
-        }
-        USART_DATA_BUFFER[USART_DATA_INDEX] = data;
-        USART_DATA_INDEX++;
-    }
-};
-
-volatile static char USART::USART_DATA_BUFFER[32] = "";
-volatile static uint8_t USART::USART_DATA_INDEX = 0;
-volatile static bool USART::USART_BUFFER_READY = false;
-
-/*
-    Function name: ISR(USART_RX_vect)
-    Logic: Whenever data is available it is appended to the buffer if the buffer has already been read 
- */
-ISR(USART_RX_vect)
-{
-    if (USART::bufferReady() == false)
-    {
-        char data = UDR0;
-        USART::updateBuffer(data);
-    }
-}
 
 int main()
 { 
